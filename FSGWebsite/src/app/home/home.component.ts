0bbf350da777d1cccc2501b9fc88ffd1c206a39d
Sidney Hopson <sidneydhopson@gmail.com>
2023-12-28 17:29:56 -0500

    Added new icon, updated website title, added mailchimp archive to news page, added basic about us description

--- FSGWebsite/src/app/home/home.component.ts
+++ FSGWebsite/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {}
+export class HomeComponent {
+  aboutUsContent = `Fried Squid Games is an indie game development studio located in Pittsburgh, Pennsylvania, USA.
+  Founded in the summer of 2022, our only goal is to create fun experiences for players to enjoy with us.
+  We develop games on PC platforms with the potential for virtual reality (VR) titles as well.
+  For those of you who are curious, our name originates from the founders' childhood nicknames: \"Short Fry\" and \"Sid the Squid\".`
+}
