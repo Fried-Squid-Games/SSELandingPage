86da2f4b7d6576e2d8ed05240d50d1c4bec51f1a
Sidney Hopson <sidneydhopson@gmail.com>
2024-01-06 13:41:58 -0500

    Remove Unused Assets, Add Steam Logo, Build Website

--- FSGWebsite/src/app/youtube-player/youtube-player.component.ts
+++ FSGWebsite/src/app/youtube-player/youtube-player.component.ts
@@ -1,48 +1,12 @@
 import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnDestroy,
-  ViewChild,
+  Component
 } from '@angular/core';
-
-declare const YT: any;
 
 @Component({
   selector: 'app-youtube-player',
   templateUrl: './youtube-player.component.html',
   styleUrls: ['./youtube-player.component.scss'],
 })
-export class YoutubePlayerComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('player') playerElement!: ElementRef;
-  playerReady = false;
-  private player: any;
+export class YoutubePlayerComponent {
 
-  ngAfterViewInit(): void {
-    if (this.playerElement && this.playerElement.nativeElement) {
-      this.loadYoutubePlayer();
-    }
-  }
-  loadYoutubePlayer(): void {
-    if (typeof YT !== 'undefined' && YT && YT.Player) {
-      this.player = new YT.Player(this.playerElement.nativeElement, {
-        height: '360',
-        width: '640',
-        videoId: 'OLe2mpiMvc0', // Replace 'VIDEO_ID' with the actual YouTube video ID
-        events: {
-          onReady: this.onPlayerReady.bind(this),
-        },
-      });
-    }
-  }
-
-  onPlayerReady(event: any): void {
-    this.playerReady = true;
-  }
-
-  ngOnDestroy(): void {
-    if (this.player) {
-      this.player.destroy();
-    }
-  }
 }
